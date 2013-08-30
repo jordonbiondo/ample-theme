92efff627a9b052286904cf8e78984d4e4513cdd
Jordon Biondo <jordon@Jordons-MacBook-Pro.local>
2013-08-30 00:15:00 -0400

    updated to a 24 theme

--- ample-theme.el
+++ ample-theme.el
@@ -5,13 +5,11 @@
 ;; Author: Jordon Biondo <biondoj@mail.gvsu.edu>
 ;; Created: Wed Jul 24 01:04:58 2013 (-0400)
 ;; Version: 0.12
-;; Last-Updated: Tue Aug  6 11:20:26 2013 (-0400)
+;; Last-Updated: Fri Aug 30 00:11:31 2013 (-0400)
 ;;           By: Jordon Biondo
-;;     Update #: 3
+;;     Update #: 5
 ;; URL: https://github.com/jordonbiondo/ample-theme
-;; Doc URL:
 ;; Keywords: theme, dark
-;; Package-Requires: ((color-theme "6.5.5"))
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;
 ;;; Commentary:
@@ -19,11 +17,6 @@
 ;;  The ample theme is a calm dark theme.
 ;;
 ;;  Currently only full colored GUI support is provided, terminal support is not implemented
-;;
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;;
-;;; Change Log:
-;;
 ;;
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;
@@ -45,92 +38,105 @@
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;
 ;;; Code:
+(deftheme ample "A smooth dark theme.")
+
+;; Not a bad idea to define a palette...
+(let ((ample/green "#6aaf50")
+      (ample/blue "#5180b3")
+      (ample/light-blue "#528fd1")
+      (ample/orange "#FF8512")
+      (ample/tan "#bdbc61")
+      (ample/dark-tan "#7d7c61")
+      (ample/yellow "#baba36")
+      (ample/bright-yellow "#fffe0a")
+      (ample/purple "#9b55c3")
+      (ample/gray "#757575")
+      (ample/red "#cd5542")
+      
+      (ample/cursor "#f57e00")
+      (ample/fringe "#1f1f1f")
+      (ample/region "#303030")
+      
+      (ample/bg "gray13")
+      (ample/fg "#bdbdb3"))
+  
+  
+  
+  ;; Set faces
+  (custom-theme-set-faces
+   `ample ;; you must use the same theme name here...
+   `(default ((t (:foreground ,ample/fg, :background ,ample/bg))))
+   `(cursor  ((t (:background ,ample/cursor))))
+   `(fringe  ((t (:background ,ample/fringe))))
+   `(region  ((t (:background ,ample/region))))
+   
+   `(font-lock-builtin-face ((t (:foreground ,ample/light-blue))))
+   `(font-lock-comment-face ((t (:foreground ,ample/gray))))
+   `(font-lock-function-name-face ((t (:foreground ,ample/green))))
+   `(font-lock-keyword-face ((t (:foreground ,ample/blue))))
+   `(font-lock-string-face ((t (:foreground ,ample/tan))))
+   `(font-lock-preprocessor-face ((t (:foreground ,ample/orange))))
+   `(font-lock-type-face ((t (:foreground ,ample/red))))
+   `(font-lock-constant-face ((t (:foreground ,ample/purple))))
+   `(font-lock-warning-face ((t (:foreground "red" :bold t))))
+   `(font-lock-variable-name-face ((t (:foreground ,ample/yellow))))
+   `(font-lock-doc-face ((t (:foreground ,ample/dark-tan))))
+   
+   `(mode-line ((t (:background "#454545"))))
+   
+   `(hl-line ((t (:background "#1f1f1f"))))
+   `(mode-line-inactive ((t (:background "cornsilk4"))))
+   
+   `(minibuffer-prompt ((t (:foreground ,ample/bright-yellow :bold t))))
+   
+   ;; eshell
+   `(eshell-ls-directory((t (:foreground ,ample/blue))))
+   `(eshell-ls-product ((t (:foreground ,ample/purple))))
+   `(eshell-ls-backup ((t (:foreground "#454545"))))
+   `(eshell-ls-executable ((t (:foreground ,ample/green))))
+   
+   ;; shell
+   `(comint-highlight-prompt ((t (:foreground ,ample/green))))
+   
+   ;; erc
+   `(erc-nick-default-face ((t (:foreground ,ample/blue))))
+   `(erc-notice-face ((t (:foreground ,ample/green))))
+   `(erc-input-face ((t (:foreground "white"))))
+   `(erc-timestamp-face ((t (:foreground "#454545"))))
+   `(erc-prompt-face ((t (:foreground "#191919" :background ,ample/purple))))
+   
+   ;; Magit
+   `(magit-branch ((t (:foreground ,ample/orange))))
+   `(magit-header ((t (:foreground ,ample/blue))))
+   `(magit-section-title ((t (:foreground ,ample/blue))))
+   `(magit-log-sha1 ((t (:foreground ,ample/purple))))
+   `(magit-item-highlight ((t (:foreground nil :background "#162433"))))
+   
+   
+   ;; error
+   `(error ((t (:foreground "red"))))
+   
+   ;; ido
+   `(ido-only-match ((t (:foreground ,ample/green))))
+   `(ido-first-match ((t (:foreground ,ample/blue))))
+   `(ido-incomplete-regexp ((t (:foreground ,ample/red))))
+   `(ido-subdir ((t (:foreground ,ample/yellow))))
+   
+   ;; trailing whitespace
+   `(trailing-whitespace ((t (:background "white" :bold t))))))
+
+;;;###autoload
+(when load-file-name
+  (add-to-list 'custom-theme-load-path
+               (file-name-as-directory (file-name-directory load-file-name))))
 
 
-(require 'color-theme)
-
-
-(defun ample-theme ()
-  "Apply the ample theme."
+(defun ample-theme() 
+  "Apply the ample-theme."
   (interactive)
-  (color-theme-install
-   '(ample-theme
-     ;;((background-color . "#14171a")
-     ((background-color . "gray13")
-      (background-mode . light)
-      (border-color . "#1f1f1f")
-      (cursor-color . "#f57e00")
-      (foreground-color . "#bdbdb3")
-      (mouse-color . "black"))
-     (fringe ((t (:background "#1f1f1f"))))
-
-     (mode-line-inactive ((t (:foreground "#191919" :background "cornsilk4"))))
-     (mode-line ((t (:foreground "#191919" :background "cornsilk3"))))
-
-     (region ((t (:background "#303030"))))
-
-     (font-lock-builtin-face ((t (:foreground "#528fd1"))))
-     ;;(font-lock-comment-face ((t (:foreground "#454545"))))
-     (font-lock-comment-face ((t (:foreground "#757575"))))
-     ;;(font-lock-comment-face ((t (:foreground "gray60"))))
-     (font-lock-function-name-face ((t (:foreground "#6aaf50"))))
-     (font-lock-keyword-face ((t (:foreground "#5180b3"))))
-     (font-lock-string-face ((t (:foreground "#bdbc61"))))
-     (font-lock-preprocessor-face ((t (:foreground "#FF8512"))))
-     (font-lock-type-face ((t (:foreground"#cd5542"))))
-     (font-lock-constant-face ((t (:foreground "#9b55c3"))))
-     (font-lock-warning-face ((t (:foreground "red" :bold t))))
-
-     (mode-line ((t (:background "#454545"))))
-     ;;(hl-line ((t (:background "gray15"))))
-     (hl-line ((t (:background "#1f1f1f"))))
-     (mode-line-inactive ((t (:background "cornsilk4"))))
-     (font-lock-variable-name-face ((t (:foreground "#baba36"))))
-     (minibuffer-prompt ((t (:foreground "#fffe0a" :bold t))))
-
-     ;; eshell
-     (eshell-ls-directory((t (:foreground "#5180b3"))))
-     (eshell-ls-product ((t (:foreground "#9b55c3"))))
-     (eshell-ls-backup ((t (:foreground "#454545"))))
-     (eshell-ls-executable ((t (:foreground "#6aaf50"))))
-
-     ;; shell
-     (comint-highlight-prompt ((t (:foreground "#6aaf50"))))
-
-     ;; erc
-     (erc-nick-default-face ((t (:foreground "#5180b3"))))
-     (erc-notice-face ((t (:foreground "#6aaf50"))))
-     (erc-input-face ((t (:foreground "white"))))
-     (erc-timestamp-face ((t (:foreground "#454545"))))
-     (erc-prompt-face ((t (:foreground "#191919" :background "#9b55c3"))))
-
-     ;; Magit
-     (magit-branch ((t (:foreground "#FF8512"))))
-     (magit-header ((t (:foreground "#5180b3"))))
-     (magit-section-title ((t (:foreground "#5180b3"))))
-     (magit-log-sha1 ((t (:foreground "#9b55c3"))))
-     (magit-item-highlight ((t (:foreground nil :background "#162433"))))
+  (load-theme 'ample t))
 
 
-     ;; error
-     (error ((t (:foreground "red"))))
-
-     ;; ido
-     (ido-only-match ((t (:foreground "#6aaf50"))))
-     (ido-first-match ((t (:foreground "#5180b3"))))
-     (ido-incomplete-regexp ((t (:foreground "#cd5542"))))
-     (ido-subdir ((t (:foreground "#baba36"))))
-
-     ;; trailing whitespace
-     (trailing-whitespace ((t (:background "white" :bold t)))))))
-
-
-;;---------------------------------------------------------------------------
-;; Add to color theme list
-;;---------------------------------------------------------------------------
-(add-to-list 'color-themes '(ample-theme "Ample" "Jordon Biondo <biondoj@mail.gvsu.edu>"))
-
-
-(provide 'ample-theme)
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;;; ample-theme.el ends here
+(provide-theme 'ample)
+;; ;;;;;;;;;;;;;;;;;;;;;;;;
+;; ample-theme.el ends here.
