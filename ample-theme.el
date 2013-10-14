fe61153e97820c1289e2d886c08262b20fd6fadc
jordonbiondo <biondoj@mail.gvsu.edu>
2013-10-14 15:12:50 -0400

    erc

--- ample-theme.el
+++ ample-theme.el
@@ -128,6 +128,8 @@
    
    ;; erc
    `(erc-nick-default-face ((t (:foreground ,ample/blue))))
+   `(erc-my-nick-face ((t (:foreground ,ample/yellow))))
+   `(erc-current-nick-face ((t (:foreground ,ample/light-blue))))
    `(erc-notice-face ((t (:foreground ,ample/green))))
    `(erc-input-face ((t (:foreground "white"))))
    `(erc-timestamp-face ((t (:foreground ,ample/darker-gray))))
